635b0d9bf574b41b35592bde972011a57301b720
boratanrikulu <boratanrikulu@gmail.com>
2020-02-28 12:06:13 +0300

    Add symmetric_keys to schema

--- schema.go
+++ schema.go
@@ -14,12 +14,22 @@ type User struct {
 type Mail struct {
   gorm.Model
   From User `gorm:"foreignkey:FromRefer"`
-  FromRefer uint
+  FromRefer uint `gorm:"not null"`
   To User `gorm:"foreignkey:ToRefer"`
-  ToRefer uint
-  Body string
+  ToRefer uint `gorm:"not null"`
+  Body string `gorm:"not null"`
+  IsEncrypted bool `gorm:"not null;default:false"`
+}
+
+type SymmetricKey struct {
+  gorm.Model
+  Sender User `gorm:"foreignkey:SenderRefer;"`
+  SenderRefer uint `gorm:"not null"`
+  Receiver User `gorm:"foreignkey:ReceiverRefer;"`
+  ReceiverRefer uint `gorm:"not null"`
+  Key string `gorm:"not null"`
 }
 
 func Migrate(db *gorm.DB) {
-  db.AutoMigrate(&User{}, &Mail{})
+  db.AutoMigrate(&User{}, &Mail{}, &SymmetricKey{})
 }
